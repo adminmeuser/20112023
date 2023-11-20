cefa70ec70a033d70d72ccd3ff5e4ec71b35428b
adminmeuser <146311162+adminmeuser@users.noreply.github.com>
2023-11-21 00:17:20 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './layout/home/home.component';
+import { AboutComponent } from './layout/about/about.component';
+import { ContactComponent } from './layout/contact/contact.component';
+import { WilscardComponent } from './wilscard/wilscard.component';
+import { StudentComponent } from './layout/student/student.component';
+// import { ProductsComponent } from './products/products.component';
+// import { ProductDetailsComponent } from './products/product-details/product-details.component';
+
+
+// import { ChildComponent } from './child/child.component';
+// import { CompleteComponent } from './complete/complete.component';
+// import { ContentComponent } from './content/content.component';
+// import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    WilscardComponent,
+    StudentComponent,
+    // ProductsComponent,
+    // ProductDetailsComponent,
+  
+    // ChildComponent,
+    // CompleteComponent,
+    // ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+    // FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
